189fd0761fa07d62cf927b9c07a5c183314e7d83
Zhao Han <cx.chenghai+github@gmail.com>
2013-03-10 20:50:44 +0000

    web2py routes changes for appadmin
    - so we can go to https://[your public dns]/4350api/appadmin to CRUD our database

--- web2py/routes.py
+++ web2py/routes.py
@@ -1,9 +1,19 @@
 routes_out = (
+
+  ### 4350 app
   ('/4350app/(?P<any>.*)', '/\g<any>'),
+  
+  ### 4350 api
+  
+  # in https
+  ('/4350api/appadmin(?P<any>.*)', '/4350api/appadmin\g<any>'),
+  # in http
   ('/4350api/(?P<any>.*)', '/\g<any>'),
+
 )
 
 routes_in = (
   ('/4350api/', '/4350api/default/index'),
+  ('/4350api/appadmin(?P<any>.*)', '/4350api/appadmin\g<any>'),
   ('/4350api/(?P<any>.*)', '/4350api/default/api/\g<any>'),
 )
